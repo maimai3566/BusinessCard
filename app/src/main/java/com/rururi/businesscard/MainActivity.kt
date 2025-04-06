b588b7ddfe92fe3725ce6f8c57c3c4aa01b3f9fb
user <ru2021kko@gmail.com>
2025-04-06 20:40:01 +0900

    初回UP

--- app/src/main/java/com/rururi/businesscard/MainActivity.kt
+++ app/src/main/java/com/rururi/businesscard/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -39,8 +43,8 @@ class MainActivity : ComponentActivity() {
 fun BusinessCardApp(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .padding(start = 16.dp,top = 100.dp,end = 16.dp,bottom = 16.dp)
-            .background(color=Color.Magenta)
+            .padding(start = 16.dp, top = 120.dp, end = 16.dp, bottom = 16.dp)
+//            .background(color = Color.Magenta)
             .fillMaxSize(),
     ) {
         Text(text = "株式会社")
@@ -68,10 +72,42 @@ fun BusinessCardApp(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
+        Spacer(modifier = Modifier.height(120.dp))
+        ContactAddress()
     }
 }
 
+@Composable
+fun ContactAddress(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+//            .background(color = Color.Yellow) //一旦黄色にしておく後で消す
+            .fillMaxWidth()                 //横幅いっぱいに広げる
+            .padding(16.dp)                 //余白
+    ) {
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Icon(imageVector = Icons.Filled.Place, contentDescription="住所")
+            Text(text = "愛知県名古屋市港区某99999",modifier=Modifier.padding(start=8.dp))
+        }
 
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Icon(imageVector = Icons.Filled.Email, contentDescription = "メール")
+            Text(text = "teck_rucco@xxxxx.xx.jp",modifier=Modifier.padding(start=8.dp))
+        }
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Icon(imageVector = Icons.Filled.Call, contentDescription = "電話番号")
+            Text(text = "090-9999-9999",modifier=Modifier.padding(start=8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactAddressPreview() {
+    BusinessCardTheme {
+        ContactAddress()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
